4d5060d68f6149b9619cf690e48416d78c4ec4ed
Jesus Miguel Garcia <alu2015508@a22o27.local>
2018-10-31 18:38:43 +0100

    Initial Commit

---
+++ cambioMoneda/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  cambioMoneda
+//
+//  Created by Jesus Miguel Garcia on 31/10/18.
+//  Copyright © 2018 Jesus Miguel Garcia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ cambioMonedaTests/cambioMonedaTests.swift
@@ -0,0 +1,34 @@
+//
+//  cambioMonedaTests.swift
+//  cambioMonedaTests
+//
+//  Created by Jesus Miguel Garcia on 31/10/18.
+//  Copyright © 2018 Jesus Miguel Garcia. All rights reserved.
+//
+
+import XCTest
+@testable import cambioMoneda
+
+class cambioMonedaTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
